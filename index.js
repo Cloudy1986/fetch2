d0705f6e3b05da79d6797097ae498fc2714047b7
Mark McLeod <markmcleod45@gmail.com>
2021-12-01 18:28:04 +0000

    Finished exercise

--- index.js
+++ index.js
@@ -5,21 +5,49 @@ const buttonEl = document.querySelector('#get-info-button');
 
 
 buttonEl.addEventListener('click', () => {
-  const userInput = inputEl.value;
+  let userInput = inputEl.value;
   
-
   fetchGithubRepo(userInput, (repoData) => {
-
     const apiResponseDiv = document.querySelector('#api-response');
+
+    // Getting the respository name
+    const nameRepoValue = repoData.name;
+    const nameElement = document.createElement('p');
+    nameElement.innerText = `Repository name: ${nameRepoValue}`;
+    apiResponseDiv.append(nameElement);
+
+    // Getting the Github url
+    const urlValue = repoData.html_url;
+    const urlElement = document.createElement('p');
+    urlElement.innerText = `Github URL: ${urlValue}`;
+    apiResponseDiv.append(urlElement);
+
+    // Getting the number of forks
     const forksValue = repoData.forks;
-    
     const forkElement = document.createElement('p');
     forkElement.innerText = `Number of forks: ${forksValue}`;
     apiResponseDiv.append(forkElement);
 
-    // console.log(repoData.forks);
-    // document.write(JSON.stringify(repoData.forks));
-    
+    // Getting the number of forks
+    const starValue = repoData.stargazers_count;
+    const starElement = document.createElement('p');
+    starElement.innerText = `Number of stargazers: ${starValue}`;
+    apiResponseDiv.append(starElement);
+
+    // Getting the number of forks
+    const languageValue = repoData.language;
+    const languageElement = document.createElement('p');
+    languageElement.innerText = `Programming language: ${languageValue}`;
+    apiResponseDiv.append(languageElement);
+
+    // Getting the repo image
+    const imageValue = repoData.owner.avatar_url;
+    const imageElement = document.createElement('img');
+    imageElement.src = imageValue;
+    apiResponseDiv.append(imageElement);
+
+    console.log(repoData);
+
   });
   
 });
