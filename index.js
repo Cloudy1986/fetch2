7d311f4e86389c3dfe38f7dbbcf85cded24b5e1c
Mark McLeod <markmcleod45@gmail.com>
2021-12-01 15:30:28 +0000

    Driver/navigator switch

---
+++ index.js
@@ -0,0 +1,6 @@
+const fetchGithubRepo = require('./fetchGithubRepo');
+
+fetchGithubRepo('sinatra/sinatra', (repoData) => {
+  console.log(repoData);
+  document.write(JSON.stringify(repoData));
+});
